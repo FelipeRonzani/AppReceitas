9a27c199a455ce94a81e7c3707a2c9fdd5f2bb0d
Felipe Ronzani <felipe.ronzani@hotmail.com>
2024-11-08 20:03:38 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,10 +48,10 @@ dependencies {
     implementation(libs.kotlin.stdlib)
 
     // Room Dependencies
-    implementation("androidx.room:room-runtime:2.6.1")
-    implementation(libs.androidx.room.common) // Room Runtime
-    kapt("androidx.room:room-compiler:2.6.1") // Room Compiler (kapt)
-    implementation("androidx.room:room-ktx:2.6.1")
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.common)
+    kapt(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
 
     // Unit Testing
     testImplementation(libs.junit)
